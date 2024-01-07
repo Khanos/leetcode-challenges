39defe309430a60cf83cd72626c1616a15be3e36
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2024-01-07 14:31:54 -0400

    Fastest solution from leetcode

--- challengues/longest-common-prefix/index.js
+++ challengues/longest-common-prefix/index.js
@@ -18,24 +18,48 @@
 //   return prefix;
 // };
 
+// /**
+//  * Second attempt
+//  * @param {string[]} strs
+//  * @return {string}
+//  */
+// var longestCommonPrefix = function(strs) {
+//   if (strs.length === 0) return "";
+//   // Assume the first string is the prefix
+//   let prefix = strs.sort[0];
+//   // check if the prefix is in the other strings
+//   for (let i = 1; i < strs.length; i++) {
+//       // while the prefix is not in the string
+//       while (strs[i].indexOf(prefix) !== 0) {
+//           // remove the last character of the prefix
+//           prefix = prefix.substring(0, prefix.length - 1);
+//           // if the prefix is empty, return empty string
+//           if (prefix === "") return "";
+//       }
+//   }
+//   return prefix;
+// };
+
 /**
- * Second attempt
+ * fastest attempt
  * @param {string[]} strs
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-  if (strs.length === 0) return "";
-  // Assume the first string is the prefix
-  let prefix = strs.sort[0];
-  // check if the prefix is in the other strings
-  for (let i = 1; i < strs.length; i++) {
-      // while the prefix is not in the string
-      while (strs[i].indexOf(prefix) !== 0) {
-          // remove the last character of the prefix
-          prefix = prefix.substring(0, prefix.length - 1);
-          // if the prefix is empty, return empty string
-          if (prefix === "") return "";
+  let prefix = '';
+  const firstStr = strs[0];
+  // for each character in the first string
+  for (let i = 0; i < firstStr.length; i++) {
+    const currChar = firstStr[i];
+    // for each string in the array
+    for (let j = 0; j < strs.length; j++) {
+      // if the character is not equal to the current character
+      // return the prefix, this breaks the loop
+      if (strs[j][i] !== currChar) {
+        return prefix;
       }
+    }
+    prefix += currChar;
   }
   return prefix;
 };
