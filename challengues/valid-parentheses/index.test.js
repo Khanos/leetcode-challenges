05d34644459441fed2210ecd4da762e8d0ea4903
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2024-01-10 08:06:58 -0400

    Adding new challenge

---
+++ challengues/valid-parentheses/index.test.js
@@ -0,0 +1,35 @@
+const isValid = require('./index');
+
+describe('Valid Parentheses', () => {
+  test('() is valid', () => {
+    expect(isValid('()')).toBe(true);
+  });
+
+  test('()[]{} is valid', () => {
+    expect(isValid('()[]{}')).toBe(true);
+  });
+
+  test('(] is not valid', () => {
+    expect(isValid('(]')).toBe(false);
+  });
+
+  test('([)] is not valid', () => {
+    expect(isValid('([)]')).toBe(false);
+  });
+
+  test('{[]} is valid', () => {
+    expect(isValid('{[]}')).toBe(true);
+  });
+
+  test('empty string is valid', () => {
+    expect(isValid('')).toBe(true);
+  });
+
+  test('single character is not valid', () => {
+    expect(isValid('[')).toBe(false);
+  });
+
+  test('single character is not valid', () => {
+    expect(isValid(']')).toBe(false);
+  });
+});
