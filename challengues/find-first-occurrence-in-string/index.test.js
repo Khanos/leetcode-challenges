0de6f19a7c6e207c2a7b8f4d6b7ff5ea9e17553d
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2024-01-22 12:48:47 -0400

    configuring github actions

--- challengues/find-first-occurrence-in-string/index.test.js
+++ challengues/find-first-occurrence-in-string/index.test.js
@@ -2,10 +2,8 @@ const strStr = require('./index');
 
 describe('challengues/find-first-occurrence-in-string', () => {
   it('should return 2', () => {
-    console.time('Execution Time');
     const result = strStr('laSJKHDZX,MNCVBLSKADJFHEOQWUPRYDSKLJFGHCMNZVBADESKJFGHEUIOWRYSDKLJFGCXMZNVBSALDKHJFGHIOWEURSLDKFGHJCXMNVBSDKLAJFGHJIEUGHFRLKSDFJCMNXBVASLDKJFASDKJFGHhello', 'll');
     expect(result).toEqual(151);
-    console.timeEnd('Execution Time');
   });
 
   it('should return -1', () => {
