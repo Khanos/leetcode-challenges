452e79456efd36a8424a8344956b0ac391256474
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2024-09-27 15:53:51 +0000

    finish a simple one to test module configs

--- challengues/running-sum-of-1d-array/index.js
+++ challengues/running-sum-of-1d-array/index.js
@@ -1,3 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
 export default function runningSum(nums) {
-  return false
+  const result = [nums[0]];
+  for (let i = 1; i < nums.length; i++) {
+    result.push(result[i-1] + nums[i])
+  }
+  return result;
 }
--- challengues/running-sum-of-1d-array/index.test.js
+++ challengues/running-sum-of-1d-array/index.test.js
@@ -1,7 +1,9 @@
 import runningSum from './index.js'
 
 describe('runningSum', () => {
-  it('should fail - This is just for debuggin', () => {
-    expect(true).toBe(false);
+  it('should return the running sum of the array', () => {
+    const nums = [1, 2, 3, 4];
+    const result = runningSum(nums);
+    expect(result).toEqual([1, 3, 6, 10]);
   });
 });
