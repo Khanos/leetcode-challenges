cc96bda62e986eb086003cab8690ab18f17fa97f
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2024-09-30 02:16:24 +0000

    HOTFIX: Adding configs to ignore node_modules and coverage folder from the linter

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,23 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {languageOptions: { 
-    globals: {
-      ...globals.browser,
-      ...globals.jest,
-    }
-  }},
+  {
+    languageOptions: { 
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
+      }
+    },
+    ignores: [
+      "coverage/*",    // Ignore coverage folder
+      "node_modules/*", // Ignore node_modules folder
+      "dist/*",        // Ignore dist folder
+      "build/*",       // Ignore build folder
+      "*.min.js",     // Ignore minified JavaScript files
+      "*.bundle.js",  // Ignore bundled JavaScript files
+      "*.config.js",  // Ignore configuration files
+    ],
+  },
   pluginJs.configs.recommended,
 ];
