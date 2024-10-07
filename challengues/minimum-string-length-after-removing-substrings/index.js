b11ea0cc5197bd25d9a88301423bf12cb01e5abe
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2024-10-07 14:13:59 +0000

    Resolving and performance refactor

--- challengues/minimum-string-length-after-removing-substrings/index.js
+++ challengues/minimum-string-length-after-removing-substrings/index.js
@@ -3,7 +3,28 @@
  * @return {number}
  */
 var minLength = function(s) {
-    return s;
+  if(s.includes('AB')) {
+    return minLength(s.replace('AB', ''));
+  }
+  if(s.includes('CD')) {
+    return minLength(s.replace('CD', ''));
+  }
+  return s.length;
 };
 
-export default minLength;
+var minLength2 = function(s) {
+  let result = s;
+  while (result.includes('AB') || result.includes('CD')) {
+    if(result.includes('AB')) {
+      result = result.replace('AB', '');
+    } else {
+      result = result.replace('CD', '');
+    }
+  }
+  return result.length;
+};
+
+export {
+  minLength,
+  minLength2,
+};
--- challengues/minimum-string-length-after-removing-substrings/index.test.js
+++ challengues/minimum-string-length-after-removing-substrings/index.test.js
@@ -1,18 +1,21 @@
-import minLength from ".";
+import {
+  minLength,
+  minLength2,
+} from ".";
+
+const testCases = [
+  ["ABFCACDB", 2],
+  ["ACBBD", 5],
+];
+
+const methods = [minLength, minLength2];
 
 describe("Minimum String Length After Removing Substrings", () => {
-  it('should return 2 for "ccdaabcdbb"', () => {
-    const s = "ccdaabcdbb";
-    expect(minLength(s)).toBe(2);
-  });
-
-  it('should return 0 for "aabccabba"', () => {
-    const s = "aabccabba";
-    expect(minLength(s)).toBe(0);
-  });
-
-  it('should return 1 for "aabbaaxybbaabb"', () => {
-    const s = "aabbaaxybbaabb";
-    expect(minLength(s)).toBe(1);
+  methods.forEach((method, index) => {
+    test(`Method ${index}`, () => {
+      testCases.forEach(([s, expected]) => {
+        expect(method(s)).toBe(expected);
+      });
+    });
   });
 });
