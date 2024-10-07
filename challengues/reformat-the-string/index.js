261a87472d4061d53dada1ccbfcbc8ba9335e3df
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2024-10-07 08:35:43 -0400

    [NEW] Reformat the string (#12)
    * First attempt
    
    * Updating description and readme

---
+++ challengues/reformat-the-string/index.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const reformat = function(s) {
+  if (s.length === 1) return s;
+  const numbers = s.match(/\d/g) || [];
+  const letters = s.match(/[a-z]/g) || [];
+  if (numbers.length === 0 || letters.length === 0) return "";
+  if (Math.abs(numbers.length - letters.length) > 1) return "";
+  
+  let first, second;
+  if (numbers.length > letters.length) {
+    first = numbers;
+    second = letters;
+  } else {
+    first = letters;
+    second = numbers;
+  }
+  
+  let result = '';
+  for (let i = 0; i < s.length; i++) {
+    result += i % 2 === 0 ? first.shift() : second.shift();
+  }
+  
+  return result;
+};
+
+export default reformat;
